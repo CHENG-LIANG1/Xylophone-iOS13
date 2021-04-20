81187ef3c903540c34ebdef287edaa9858ea1e3c
CHENG-LIANG1 <madfoxfoxy@gmail.com>
2021-04-20 12:22:47 +0800

    Added button dimming effect

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -9,10 +9,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 
-    @IBAction func keyPressed(_ sender: UIButton) {
-        playSound(key: sender.currentTitle!)
-
-    }
     
     func playSound(key: String) {
         let url = Bundle.main.url(forResource: key, withExtension: "wav")
@@ -20,5 +16,15 @@ class ViewController: UIViewController {
         player.play()
                 
     }
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(key: sender.currentTitle!)
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
+    }
+
 }
 
